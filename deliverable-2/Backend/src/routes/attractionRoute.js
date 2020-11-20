3794806cfe1a9ebdd2e71079d8dcc12aac8a1985
Zhiyuan Sun <32525238+ZhiYuanS@users.noreply.github.com>
2020-11-20 10:01:30 -0500

    Fix the output of AttractionRoute

--- deliverable-2/Backend/src/routes/attractionRoute.js
+++ deliverable-2/Backend/src/routes/attractionRoute.js
@@ -7,12 +7,10 @@ router.get('/', function(req, res){
     Attraction.find()
         .then((result) => {
             res.send(result)
-            attractionList.push(result)
         })
         .catch((err) => {
             console.log(err);
         });
-    return attractionList
 })
 
 module.exports = router;
