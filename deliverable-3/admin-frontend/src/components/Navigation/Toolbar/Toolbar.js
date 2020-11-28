308f77dd36c5b45863b830ee422cda8c2fb1b754
francomomo <francomomo100@gmail.com>
2020-11-28 17:29:06 -0500

    Implementing navigation

--- deliverable-3/admin-frontend/src/components/Navigation/Toolbar/Toolbar.js
+++ deliverable-3/admin-frontend/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Container from 'react-bootstrap/Container'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 
 const Toolbar = () => {
     return (
         <Navbar bg='light' expand='lg'>
-            <Navbar.Brand>Ontario Parks Admin</Navbar.Brand>
-            <Nav className="mr-auto">
-                <Nav.Link><Link to='/'>Home</Link></Nav.Link>
-                <Nav.Link><Link to='/attractions'>Attractions</Link></Nav.Link>
-                <Nav.Link><Link to='/parks'>Parks</Link></Nav.Link>
-                <Nav.Link><Link to='/categories'>Categories</Link></Nav.Link>
-            </Nav>
+            <Container>
+                <Navbar.Brand>Ontario Parks Admin</Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link><Link to='/'>Home</Link></Nav.Link>
+                    <Nav.Link><Link to='/attractions'>Attractions</Link></Nav.Link>
+                    <Nav.Link><Link to='/parks'>Parks</Link></Nav.Link>
+                    <Nav.Link><Link to='/categories'>Categories</Link></Nav.Link>
+                </Nav>
+            </Container>
         </Navbar>
     );
 };
--- deliverable-3/admin-frontend/src/pages/categoryPage.js
+++ deliverable-3/admin-frontend/src/pages/categoryPage.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 
-const categoryPage = () => {
+import Container from 'react-bootstrap/Container'
+
+const CategoryPage = () => {
+    const [categories, setCategories] = useState();
+
+    useEffect(() => {
+        axios.get('http://127.0.0.1:5000/api/categories').then(res => {
+            console.log(res)
+        }).catch(err => {
+            console.log(err)
+        })
+    })
+
     return (
-        <div>
-            Category
-        </div>
+        <Container>Category</Container>
     );
 };
 
-export default categoryPage;
+export default CategoryPage;
